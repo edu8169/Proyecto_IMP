9a4eb572ab2bfc6072030dc83f2dba3e62b18817
edu8169 <62029234+edu8169@users.noreply.github.com>
2022-11-11 20:10:14 -0600

    My first commit

---
+++ PostgreSQL/pg_env.bat
@@ -0,0 +1,11 @@
+@ECHO OFF
+REM The script sets environment variables helpful for PostgreSQL
+
+@SET PATH="D:\vodoo\PostgreSQL\bin";%PATH%
+@SET PGDATA=D:\vodoo\PostgreSQL\data
+@SET PGDATABASE=postgres
+@SET PGUSER=openpg
+@SET PGPORT=5432
+@SET PGLOCALEDIR=D:\vodoo\PostgreSQL\share\locale
+
+                            
